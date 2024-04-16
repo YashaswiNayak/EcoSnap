dee47ec6a1be0db24a94e3f0f68d561788e4c4af
Yashaswi Nayak <yn6920345@gmail.com>
2024-04-16 20:26:00 +0530

    added the comment section

--- app/src/main/java/com/example/ecosnap/Comment.kt
+++ app/src/main/java/com/example/ecosnap/Comment.kt
@@ -1,6 +1,8 @@
 package com.example.ecosnap
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +18,11 @@ class Comment:AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val listView = findViewById<ListView>(R.id.listView)
+
+// Sample data for the ListView
+        val data = arrayOf("Item 1", "Item 2", "Item 3")
+
     }
 
 }
